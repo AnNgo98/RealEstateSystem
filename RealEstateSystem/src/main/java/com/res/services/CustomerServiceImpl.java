53bbd83a0cbb37424c8cf4135d637f1252bf5286
Thuy Dao Xuan <thuydx.9598@gmail.com>
2018-12-18 16:56:50 +0700

    new class

--- RealEstateSystem/src/main/java/com/res/services/CustomerServiceImpl.java
+++ RealEstateSystem/src/main/java/com/res/services/CustomerServiceImpl.java
@@ -4,10 +4,12 @@ import com.res.models.Customer;
 import com.res.models.Employee;
 import com.res.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Objects;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
